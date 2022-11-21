9fc14456e2dd5425a56af5a02e6cdae420c6a1c3
JuliaP <flaeeee@gmail.com>
2022-11-21 17:22:30 +0300

    дз не доделано

---
+++ homeworks/HW1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Task 2
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+
+Console.WriteLine(max);*/
+
+// Task 4
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 1");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine(max);*/
+
+// Task 6
+
+
+/*Console.WriteLine("Input number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n % 2) == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}*/
+
+// Task 8
+
+Console.WriteLine("Input number");
+int n = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+
+while (num < n)
+{
+    if ((num % 2) == 0)
+    {
+        Console.WriteLine(num);
+        num == num +2
+    }
+}
