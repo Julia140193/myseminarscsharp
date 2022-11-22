213e1bb7303449e60cb0b4ea94a5261b853c251b
JuliaP <flaeeee@gmail.com>
2022-11-22 15:20:21 +0300

    дз2 не доделано

---
+++ homeworks/HW2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Task 10
+
+/*int CutNumber (int num)
+{
+    int dec = num / 10;
+    int res = dec % 10;
+    return res;
+}
+Console.WriteLine("Input number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    int newnumber = CutNumber(n);
+    Console.WriteLine($"New version of {n} is {newnumber}");
+}
+else
+{
+    Console.WriteLine("Error, please, input three-digit number");
+}*/
+
+// Task 13
+
+/*int CutNumber (int num)
+{
+    int s = Convert.To
+    return dig3;
+}
+Console.WriteLine("Input number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100)
+{
+    int di3 = CutNumber(n);
+    Console.WriteLine($"The third digit of {n} is {dig3}");
+}
+else
+{
+    Console.WriteLine("Missing third digit");
+}*/
