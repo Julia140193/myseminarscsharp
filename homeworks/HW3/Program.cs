b5fe3caa600cacd44ed352a3d1413f6954020382
JuliaP <flaeeee@gmail.com>
2022-11-28 15:31:13 +0300

    HW3

---
+++ homeworks/HW3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Task 19
+
+/*bool Palindrom (int a, int b, int c, int d)
+{
+    if(a == d)
+    {
+        if(b == c)
+        {
+            return true;
+        }
+        return false;
+    }
+    else
+    {
+        return false;
+    }
+}   
+
+Console.WriteLine("Input five-digit number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 10000 || n > 99999)
+    {
+        Console.WriteLine("Error, invalid value");
+    }
+    else
+    {
+        int ed = n % 10;
+        int dec = (n / 10) % 10;
+        int th = (n / 1000) % 10;
+        int tenth =(n / 10000);
+        bool b = Palindrom(ed, dec, th, tenth);
+        Console.WriteLine(b);
+    }*/
+    
+
+    // Task 21
+
+/*double Line(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double Len = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
+    Console.WriteLine(Len);
+    return Len;
+}
+Console.WriteLine("Введите координату x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z1");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z2");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Line(x1, x2, y1, y2, z1, z2);*/
+
+// Task 23
+
+/*Console.WriteLine("Input number ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+for(int i = 1; i <= x; i++)
+     
+     {
+         int res = i * i * i;
+         Console.Write($"{res}, "); 
+     }
+     
+Console.WriteLine();*/
---
+++ homeworks/HW4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Task 25
+
+int FindDegree (int x1, int x2)
+{
+    for (int count = 1; count <= x2; count++)
+    {
+        int degree = 1;
+        degree = degree * x1;
+    }
+    return degree;
+}
+
+
+Console.WriteLine("Input number A ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B ");
+int B = Convert.ToInt32(Console.ReadLine());
+int degree = FindDegree(A,B);
+
+if (B > 0)
+{
+    FindDegree (A, B);
+    Console.WriteLine(degree);
+}
+else
+{
+    Console.WriteLine("Invalid Value");
+}
