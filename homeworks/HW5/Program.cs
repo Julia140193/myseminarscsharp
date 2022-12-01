b858fbdebf31dc3a81d1efae3bb1c40b3eef7c82
JuliaP <flaeeee@gmail.com>
2022-12-01 17:36:00 +0300

    HW6 доделано

--- homeworks/HW5/Program.cs
+++ homeworks/HW5/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 34
+
+/*void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    
+    for(int i = 0; i< count; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] EvenNumbers (int[] array)
+{
+    int count = 0;
+    for(int i = 0; i <= array.Length; i++)
+    {
+        if(array[i] % 2 ==0)
+        {
+        count = count + 1;
+        }
+    }
+    Console.WriteLine(count);
+    return array;
+}
+
+int [] array = new int[10];
+FillArray(array);
+PrintArray(array);
+EvenNumbers(array);*/
+
+// Task 36
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(-10, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    
+    for(int i = 0; i< count; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] SumofElements(int [] array)
+{
+    
+}
---
+++ homeworks/HW6/Program.cs
@@ -0,0 +1,75 @@
+﻿// Task 41
+
+/*int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for(int i = 0; i<size; i++)
+    {
+        newArray[i] = new Random(). Next(minValue, maxValue + 1);
+    }
+    return newArray;
+}
+
+void ShowArray( int [] array)
+{
+    Console.Write($"Получившийся массив -> ");
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write (array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+int [] Quantity (int [] array)
+{
+    int count = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        if(array[i] > 0) 
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine($"{count}");
+    return array;
+}
+
+Console.WriteLine("введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите минимальное значение");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите максимальное значение");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = CreateRandomArray(a, min, max);
+ShowArray(Quantity(myArray));*/
+
+// Task 43
+/*int FindPointCoordinates (int k1, int b1, int k2, int b2)
+{
+    int x = 0;
+    int y = 0;
+    if(k1 == k2)
+    {
+        Console.WriteLine("There is no point of intersection");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1*((b2 - b1) / (k1 - k2)) + b1;
+        Console.WriteLine($"({x};{y})");
+    }
+    return x;
+}
+
+
+Console.WriteLine("введите значение k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+FindPointCoordinates(k1, b1, k2, b2);*/
