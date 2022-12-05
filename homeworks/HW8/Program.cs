6bfaeec882e52693ccaf8263ccf3c356fb5dd74f
JuliaP <flaeeee@gmail.com>
2022-12-05 12:37:25 +0300

    HW9 доделано

---
+++ homeworks/HW8/Program.cs
@@ -0,0 +1,117 @@
+﻿// Task 54
+
+/*int [,] Create2RandomDArray (int rows, int columns, int minValue, int maxValue)
+{
+    int [,] newArray = new int[rows, columns]; //выделяем память для массива
+    for(int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j<columns; j++)
+        {
+            newArray[i,j] = new Random(). Next(minValue, maxValue+1);
+        }
+    }
+    return newArray;
+}
+
+
+void Show2DArray (int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int [,] ChangeElements (int [,] array)
+{
+
+}
+
+
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2RandomDArray(rows,columns, min, max);
+Show2DArray(myArray);*/
+
+// Task 56
+
+int [,] Create2RandomDArray (int rows, int columns, int minValue, int maxValue)
+{
+    int [,] newArray = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j<columns; j++)
+        {
+            newArray[i,j] = new Random(). Next(minValue, maxValue+1);
+        }
+    }
+    return newArray;
+}
+
+void Show2DArray (int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumofRows (int [,] array)
+{
+    int no = 0;
+    int sum = 0;
+    int min = 0;
+    for (int j =  sum = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[0,j];
+        int minsum = SumofRows;
+        no = 0;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = sum = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        if(sum < minsum) 
+        {
+            minsum = sum;
+            no = i;
+        }
+        Console.WriteLine($"{sum}");
+        Console.WriteLine($"{i}");
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [,] myArray = Create2RandomDArray(rows,columns, min, max);
+Show2DArray(myArray);
+Console.WriteLine();
+SumofRows(myArray);
---
+++ homeworks/HW9/Program.cs
@@ -0,0 +1,78 @@
+﻿// Task 64
+
+/*void ShowNumbers(int n)
+{
+    if(n>=1)
+    {
+        Console.Write(n+" ");
+        ShowNumbers(n-1);
+    }
+}
+Console.WriteLine ("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(n);*/
+
+// Task 66
+
+/*int FindSum (int m, int n)
+{
+   if(m == n) return m;
+   if (m > n)
+        {
+            return m + FindSum(m -1, n); 
+        }
+    else
+        {
+            return n + FindSum(m, n-1);
+        }
+}
+Console.WriteLine ("Введите число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FindSum(m,n));*/
+ 
+
+ // Task 68
+
+/*int Akkerman (int m, int n)
+{
+    if(m==0)
+    {
+        return n+1;
+    }
+    else
+    {
+        if(n==0 && m > 0)
+        {
+            return Akkerman(m-1, 1);
+        }
+        else
+        {
+            return Akkerman(m-1, Akkerman(m, n-1));
+        }
+    }
+}
+
+Console.WriteLine ("Введите число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(m >0 && n > 0)
+{
+    Console.WriteLine(Akkerman(m,n));
+}
+else
+{
+    Console.WriteLine("Введите числа больше нуля, иначе программа не заработает");
+}*/
+
+
+
+
+
+
+
+
+
